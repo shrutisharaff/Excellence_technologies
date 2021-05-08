3a3a55652e7e730ca6dcb41c8faf892fcf670f15
shrutisharaff <80566124+shrutisharaff@users.noreply.github.com>
2021-05-08 19:20:12 +0530

    Create Ques2.js

---
+++ Ques2.js
@@ -0,0 +1,20 @@
+function maxConsecutiveOnes() 
+{
+  let nums = [0,0,1,1,0,0,0,1,0,0,1,1,1,0];
+  let largestCount = 0; 
+  let currentCount = 0; 
+  for (let i = 0; i<nums.length; i++) 
+  { 
+    if (nums[i] === 0) 
+    { 
+      currentCount = 0; 
+    }
+    else 
+    {
+      currentCount++; 
+    }
+    if (currentCount > largestCount) 
+      largestCount = currentCount;
+  };
+  return largestCount;
+}
