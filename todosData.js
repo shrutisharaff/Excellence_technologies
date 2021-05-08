6da34a353dcfdcb1b361a80eab582a75cf95f154
shrutisharaff <80566124+shrutisharaff@users.noreply.github.com>
2021-05-08 19:43:25 +0530

    Add files via upload
    todo app

---
+++ todosData.js
@@ -0,0 +1,15 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Clean the desk",
+        completed: false
+    }
+
+]
+
+export default todosData
